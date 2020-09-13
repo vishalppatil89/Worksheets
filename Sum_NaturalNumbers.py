6dcb1410025d1be28abb4eed3af0ca14eae8a850
vishalppatil89 <64767185+vishalppatil89@users.noreply.github.com>
2020-09-13 21:18:47 +0530

    Add files via upload

---
+++ Sum_NaturalNumbers.py
@@ -0,0 +1,9 @@
+'''14. Write a python program to solve a quadratic equation. Where a, b and c are to 
+be taken by user input. Handle the erroneous input, such as ‘a’ should not be equal to 0. '''
+
+
+a=int(input("Enter the natural number "))
+
+sum=(a*(a+1)/2)
+
+print(sum)
---
+++ roots_QE.py
@@ -0,0 +1,27 @@
+'''15. Write a python program to find the sum of first ‘n’ natural numbers without using any loop. Ask user to input 
+the value of ‘n’.''' 
+
+
+
+
+from math import sqrt
+
+print("To find roots of Quadratic function : (a * x^2) + b*x + c")
+a = float(input("a: "))
+b = float(input("b: "))
+c = float(input("c: "))
+
+r = b**2 - 4*a*c
+
+if r > 0:
+    num_roots = 2
+    x1 = (((-b) + sqrt(r))/(2*a))     
+    x2 = (((-b) - sqrt(r))/(2*a))
+    print("There are 2 roots: %f and %f" % (x1, x2))
+elif r == 0:
+    num_roots = 1
+    x = (-b) / 2*a
+    print("There is one root: ", x)
+else:
+    num_roots = 0
+    print("No roots, discriminant < 0.")
